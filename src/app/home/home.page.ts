acdff2e5618baba0bb631f00ea9f52e3fb61b01b
Bruno <tp.bruno.silvac@totvspartners.com.br>
2024-10-05 14:40:59 -0300

    feat(home.page): adding product list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonRow,
+  IonButton,
+  IonCol,
+  IonThumbnail,
+  IonImg,
+  IonCard, IonLabel, IonText, IonIcon, IonSearchbar } from '@ionic/angular/standalone';
+import { ApiService } from '../services/api/api.service';
+import { Product } from '../interfaces/product.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonSearchbar, IonIcon, IonText, IonLabel, 
+    IonImg,
+    IonCol,
+    IonButton,
+    IonRow,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonThumbnail,
+    IonCard
+  ],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  items: Product[] = [];
+  allItems: Product[] = [];
+  api = inject(ApiService);
+  query: string = '';
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.getItems();
+  }
+
+  getItems(): void {
+    this.allItems = this.api.items;
+    this.items = [...this.allItems];
+  }
+
+  onSearchChange(event: any): void {
+    this.query = event.detail.value.toLowerCase();
+    this.querySearch();
+  }
+
+  querySearch(): void {
+    this.items = [];
+    if(this.query.length > 0) {
+      this.searchItems();
+    } else {
+      this.items = [...this.allItems];
+    }
+  }
+
+  searchItems(): void {
+    this.items = this.api.items.filter((item: Product) => item.name.toLowerCase().includes(this.query));
+  }
+  
 }
